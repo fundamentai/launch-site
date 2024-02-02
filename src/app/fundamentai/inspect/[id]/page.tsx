221b1b38a045e54996ebd0710138e40fb6b20a50
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-02-02 21:17:21 +0300

    link added

--- src/app/fundamentai/inspect/[id]/page.tsx
+++ src/app/fundamentai/inspect/[id]/page.tsx
@@ -28,10 +28,10 @@ export default function page({ params }: { params: { id: string } }) {
             <div className="flex flex-col items-center max-w-[900px] w-[60vw] gap-y-8">
                 <h1 className="text-center text-[25px] font-bold leading-relaxed">{source?.title.toUpperCase()}</h1>
                 <label>{source?.description}</label>
-                <a href="http://localhost:3000/fundamentai/inspect/2" target="_blank">
-                    <div className="flex items-center justify-center gap-x-2 text-[14px] text-black/50 px-2 py-[2px] border-2 border-solid border-black/30 rounded-xl hover:bg-slate-100 duration-100">
-                        http://localhost:3000/fundamentai/inspect/2 <ExportOutlined className="h-[14px]" />
-                    </div>
+                <a href={source?.link} target="_blank">
+                    <h1 className="flex  items-center justify-center gap-x-2 text-[14px] text-black/50 px-2 py-[2px] border-2 border-solid border-black/30 rounded-xl hover:bg-slate-100 duration-100">
+                        {source?.link.length > 90 ? source?.link.slice(0, 90) + '...' : source?.link} <ExportOutlined className="h-[14px]" />
+                    </h1>
                 </a>
                 <div className="my-4 flex flex-col gap-y-4 w-full">
                     <AddRelations />
