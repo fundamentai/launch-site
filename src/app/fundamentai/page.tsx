69a05580f48230dc5028f7be570235bc0d0e4d1d
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-02-02 18:57:42 +0300

    inpect implemented

--- src/app/fundamentai/page.tsx
+++ src/app/fundamentai/page.tsx
@@ -19,6 +19,7 @@ export default function page() {
             }
         })
     }
+    console.log(data)
 
     return (
         <div className="w-full  flex flex-col items-center justify-start">
